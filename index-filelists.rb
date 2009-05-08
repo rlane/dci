005db5fdca511b3d852da8174380bc9d51ba3b78
Rich Lane <rlane@club.cc.cmu.edu>
2009-05-08 00:08:57 -0400

    use libxml

--- index-filelists.rb
+++ index-filelists.rb
@@ -1,49 +1,59 @@
 #!/usr/bin/env ruby
 require 'rubygems'
-require 'rexml/document'
+require 'libxml'
 require 'common'
+include LibXML
 
 FILELISTS_DIR = "filelists"
 DATA_FILENAME = "data"
 
-#filelists = Dir.glob(FILELISTS_DIR + "/*")
-filelists = %w(zzazzors).map { |x| FILELISTS_DIR + "/" + x + ".filelist.xml" }
+filelists = Dir.glob(FILELISTS_DIR + "/*")
+#filelists = %w(zzazzors).map { |x| FILELISTS_DIR + "/" + x + ".filelist.xml" }
 
-def process_directory node, username, ancestor_names
-	return unless node
-	node.elements.each do |e|
-		as = e.attributes
-		path = ancestor_names + [as['Name']]
-		if e.name == 'File'
-			#puts "File " + as['Name']
-			tth = as['TTH']
-			fs_path = path * '/'
-			text_path = path * ' '
-			size = as['Size'].to_i
-			x = $index[tth] || { :locations => [], :terms => [], :texts => [], :size => nil }
-			x[:locations] << [username, fs_path]
-			x[:texts] += [text_path]
-			x[:size] = size
-			$index[tth] = x
-		elsif e.name == 'Directory'
-			#puts "Directory " + as['Name']
-			process_directory e, username, path
-		else
-			puts "Unknown node type #{e.name}"
+def process_file tth, username, path, size
+	fs_path = path * '/'
+	text_path = path * ' '
+	x = $index[tth] || { :locations => [], :terms => [], :texts => [], :size => nil }
+	x[:locations] << [username, fs_path]
+	x[:texts] += [text_path]
+	x[:size] = size
+	$index[tth] = x
+end
+
+R = XML::Reader
+def process_filelist xml, username
+	path = []
+	while (xml.read == 1)
+		name, node_type = xml.name, xml.node_type
+		if name == 'File' && node_type == R::TYPE_ELEMENT
+			path.push xml['Name']
+			process_file xml['TTH'], username, path, xml['Size'].to_i
+			path.pop
+		elsif name == 'Directory' && node_type == R::TYPE_ELEMENT
+			path.push xml['Name']
+		elsif name == 'Directory' && node_type == R::TYPE_END_ELEMENT
+			path.pop
 		end
 	end
 end
 
 $index = MarshalledGDBM.new DATA_FILENAME
+$index.cachesize = 10000
+$index.fastmode = true
 
-filelists.each do |filelist|
+i = 0
+n = filelists.length
+filelists.sort.each do |filelist|
+	i += 1
 	fail 'bad filename' unless filelist =~ /^#{FILELISTS_DIR}\/(.*).filelist.xml/
 	username = $1
 	File.open(filelist, "r") do |f|
-		puts "reading #{filelist}"
-		xml = REXML::Document.new(f.read)
+		puts "(#{i}/#{n}) processing #{filelist}"
+		xml = XML::Reader.io f
 		next if xml.nil?
-		puts "processing #{filelist}"
-		process_directory xml.root, username, []
+		process_filelist xml, username
 	end
 end
+
+puts "compacting..."
+$index.reorganize
