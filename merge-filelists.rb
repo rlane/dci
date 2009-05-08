ee721e3253aabd5d3b34029239351992b6342aaa
Rich Lane <rlane@club.cc.cmu.edu>
2009-05-08 12:32:05 -0400

    libxml fix #2

--- merge-filelists.rb
+++ merge-filelists.rb
@@ -22,7 +22,7 @@ end
 R = XML::Reader
 def process_filelist xml, username
 	path = []
-	while xml.read
+	while (ret = xml.read; (ret == 1) || (ret == true))
 		name, node_type = xml.name, xml.node_type
 		if name == 'File' && node_type == R::TYPE_ELEMENT
 			path.push xml['Name']
